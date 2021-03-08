4f14ffb0ea042d3b44cee4f2633859d34a5338fd
903958734@qq.com <903958734@qq.com>
2021-03-08 21:59:28 +0800

    初始化ââ

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Bobo/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Bobo from './components/Bobo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Bobo
+  }
+}
+</script>
