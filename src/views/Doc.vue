1e60025683ddbe870154bafa9e7e9de7214d5fb4
903958734@qq.com <903958734@qq.com>
2021-03-10 21:56:15 +0800

    aside css 1

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -24,6 +24,24 @@
     </div>
   </div>
 </template>
+<style lang = "scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
 
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
