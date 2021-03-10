697348cfeb6456588aa69668de93fdb4b3c0a739
903958734@qq.com <903958734@qq.com>
2021-03-10 21:00:11 +0800

    topNav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <div class="topBar">
-      <div class ="logo"></div>
-      <div class="menu"></div>
+    <div class="topnav">
+      <div class ="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>啵啵UI</h1>
@@ -16,7 +19,20 @@
 </template>
 
 <style lang="scss" scoped>
-  .topBar{
-    border: 1px solid red;
+  .topnav{
+    background: pink;
+    display: flex;
+    >.logo{
+      max-width: 6em;
+      margin-right: auto;
+    }
+    >.menu{
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      > li{
+        margin: 0 1em;
+      }
+    }
   }
 </style>
