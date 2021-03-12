919fb7ea4da0d8ccb140bd6b0b91ee964aebb54a
903958734@qq.com <903958734@qq.com>
2021-03-12 23:31:01 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     我是Button 文档
-
   </div>
+  <div>示例</div>
+  <Button>你好<Button>
+
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+export default {
+  components:{Button}
+}
+
+</script>
