43065053a4422a89847cc49551663af225a879e3
903958734@qq.com <903958734@qq.com>
2021-03-13 00:21:36 +0800

    给Button绑定事件属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    我是Button 文档
-  </div>
+  <div>我是Button 文档</div>
   <div>示例</div>
-  <Button>你好<Button>
+  <Button @click="onClick"
+          @focus="onClick"
+          @mouseover="onClick"
+          size="small"
+          >你好
+    <Button>
 
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue';
 export default {
-  components:{Button}
+  components:{Button},
+  setup(){
+    const onClick =()=>{
+      console.log('hi');
+    }
+    return{onClick};
+  }
 }
 
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,3 +1,25 @@
 <template>
-  <button><slot/></button>
+  <!-- 父元素传过来的所有属性默认绑定到根元素上，可以不继承，并且拆分属性只绑定部分属性 -->
+  <div :size = "size">
+    <!-- <button v-bind="$attrs"> 把父元素传过来的所有属性绑定到button上 -->
+    <button v-bind="rest"> <!-- 只绑定除size外的剩余属性 -->
+      <slot/>
+    </button>
+  </div>
 </template>
+
+<script lang="ts">
+export default {
+  inheritAttrs:false,//模板根元素不继承父元素传过来的事件
+  setup(props,context){
+    const{onClick,onMouseOver,size,...rest}=context.attrs;//拿到父元素传的属性解构赋值
+    return{onClick,onMouseOver,size,rest};
+
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
