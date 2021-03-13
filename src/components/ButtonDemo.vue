0b636b91348fd9d0460b65c9779e5715cecd4ea3
903958734@qq.com <903958734@qq.com>
2021-03-13 14:52:50 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,28 +2,47 @@
 
   <h2>示例1</h2>
   <div>
-    <Button  >你好</Button>
-    <Button theme="button" >你好</Button>
-    <Button theme="link" >你好</Button>
-    <Button theme="text" >你好</Button>
+    <Button  >默认按钮</Button>
+    <Button theme="button" >普通按钮</Button>
+    <Button theme="link" >链接按钮</Button>
+    <Button theme="text" >文字按钮</Button>
   </div>
 
   <h2>示例2</h2>
   <div>
     <div>
-      <Button size="big">大大大</Button>
-      <Button>普普通</Button>
-      <Button size="small">小小小</Button>
+      <Button size="big">大普通按钮</Button>
+      <Button>中普通按钮</Button>
+      <Button size="small">小普通按钮</Button>
     </div>
     <div>
-      <Button theme="link" size="big">大大大</Button>
-      <Button theme="link">普普通</Button>
-      <Button size="small" theme="link">小小小</Button>
+      <Button theme="link" size="big">大链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button size="small" theme="link">小链接按钮</Button>
     </div>
     <div>
-      <Button size="big" theme="text">大大大</Button>
-      <Button theme="text">普普通</Button>
-      <Button size="small" theme="text">小小小</Button>
+      <Button size="big" theme="text">大文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button size="small" theme="text">小文字按钮</Button>
+    </div>
+  </div>
+
+  <h2>示例3</h2>
+  <div>
+    <div>
+      <Button level="main">主要普通按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险普通按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
     </div>
   </div>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
       size:{
         type:String,
         default: 'normal'
-      }
+      },
+      level: {
+        type: String,
+        default: "normal",
+      },
     },
     setup(props){
-      const {theme,size} = props;
+      const {theme,size,level} = props;
       const classes = computed(()=>{
         return{
           [`gulu-theme-${theme}`]:theme,
           [`gulu-size-${size}`]:size,
+          [`gulu-level-${level}`]:level
         }
       })
       return {classes}
@@ -38,6 +43,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+
 .gulu-button {
   box-sizing: border-box;
   height: $h;
@@ -52,6 +59,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -66,12 +74,41 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.gulu-theme-button {
+    &.gulu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
   &.gulu-theme-link{
     border-color: transparent;
     box-shadow: none;
     color: $blue;
     &:hover,&:focus{
       color: lighten($blue, 10%);
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
   &.gulu-theme-text{
@@ -80,6 +117,20 @@ $radius: 4px;
     color: inherit;
     &:hover,&:focus{
       background: darken(white, 5%);;
+    }
+    &.gulu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
     }
   }
   &.gulu-size-big {
@@ -92,6 +143,7 @@ $radius: 4px;
     height: 20px;
     padding: 0 4px;
   }
+
 }
 
 
