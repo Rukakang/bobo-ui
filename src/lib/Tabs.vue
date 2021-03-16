ed59d9127a4bbda30fe4c1759478f14b95013a0f
903958734@qq.com <903958734@qq.com>
2021-03-17 00:32:29 +0800

    显示被选中的导航

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
-      <div class = "gulu-tabs-nav-item" v-for="t in titles" :key="t">{{t}}</div>
+      <div class = "gulu-tabs-nav-item" v-for="t in titles" :key="t" :class="{selected:t===selected}">{{t}}</div>
     </div>
 
     <div class="gulu-tabs-content">
@@ -15,6 +15,11 @@
 import Tab from "./Tab.vue";
 
 export default {
+  props:{
+    selected:{
+      type:String
+    }
+  },
   setup(props,context){
     const defaults = context.slots.default();
     defaults.forEach((tag)=>{
