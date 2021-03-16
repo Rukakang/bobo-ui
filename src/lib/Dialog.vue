abadba3599dc375de66f8b8b0a9077482c053cdf
903958734@qq.com <903958734@qq.com>
2021-03-16 15:09:24 +0800

    创建Dialog组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行内容</p>
+      <p>第二行内容</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+<script>
+import Button from "./Button";
+export default {
+  components: {Button}
+}
+</script>
