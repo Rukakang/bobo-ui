968ff966934cbeb0aacbda315703fe90c359c54f
903958734@qq.com <903958734@qq.com>
2021-03-18 15:51:19 +0800

    展示或隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -6,7 +6,8 @@
         <component :is="component"/>
       </div>
       <div class="demo-actions">
-        <Button @click="toggle">查看代码</Button>
+        <Button @click="hideCode" v-if="codeVisible">隐藏代码</Button>
+        <Button @click="showCode" v-else>查看代码</Button>
       </div>
       <div class="demo-code" v-if="codeVisible">
         <pre class="language-html" v-html="html" />
@@ -34,10 +35,10 @@ export default {
     const html =computed(()=>{
       return Prism.highlight(props.component.__sourceCode,Prism.languages.html,'html');
     });
-    const toggle = ()=>{
-        codeVisible.value =!codeVisible.value;
-    }
-    return {Prism,html,toggle,codeVisible}
+    const showCode = () => codeVisible.value = true
+    const hideCode = () => codeVisible.value = false
+    return {Prism,html,showCode,
+      hideCode,codeVisible}
   }
 }
 
